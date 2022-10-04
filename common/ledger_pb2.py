57b08efb6ede5d6952badb9388724c173b7828f6
Luis Lara <62342483+LuisLara-UH@users.noreply.github.com>
2022-10-04 16:24:21 -0400

    Updated Python Protocol Buffers for Python 2.3

--- common/ledger_pb2.py
+++ common/ledger_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13\x63ommon/ledger.proto\x12\x06\x63ommon\"U\n\x0e\x42lockchainInfo\x12\x0e\n\x06height\x18\x01 \x01(\x04\x12\x18\n\x10\x63urrentBlockHash\x18\x02 \x01(\x0c\x12\x19\n\x11previousBlockHash\x18\x03 \x01(\x0c\x42V\n$org.hyperledger.fabric.protos.commonZ.github.com/hyperledger/fabric-protos-go/commonb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13\x63ommon/ledger.proto\x12\x06\x63ommon\"\x9b\x01\n\x0e\x42lockchainInfo\x12\x0e\n\x06height\x18\x01 \x01(\x04\x12\x18\n\x10\x63urrentBlockHash\x18\x02 \x01(\x0c\x12\x19\n\x11previousBlockHash\x18\x03 \x01(\x0c\x12\x44\n\x19\x62ootstrappingSnapshotInfo\x18\x04 \x01(\x0b\x32!.common.BootstrappingSnapshotInfo\"8\n\x19\x42ootstrappingSnapshotInfo\x12\x1b\n\x13lastBlockInSnapshot\x18\x01 \x01(\x04\x42V\n$org.hyperledger.fabric.protos.commonZ.github.com/hyperledger/fabric-protos-go/commonb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'common.ledger_pb2', globals())
@@ -21,6 +21,8 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'\n$org.hyperledger.fabric.protos.commonZ.github.com/hyperledger/fabric-protos-go/common'
-  _BLOCKCHAININFO._serialized_start=31
-  _BLOCKCHAININFO._serialized_end=116
+  _BLOCKCHAININFO._serialized_start=32
+  _BLOCKCHAININFO._serialized_end=187
+  _BOOTSTRAPPINGSNAPSHOTINFO._serialized_start=189
+  _BOOTSTRAPPINGSNAPSHOTINFO._serialized_end=245
 # @@protoc_insertion_point(module_scope)
